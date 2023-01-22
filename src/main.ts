37bfe79efd7d02ef843caa299c49400c12c2123d
manaljav <manaljav0425@gmail.com>
2023-01-21 21:18:48 -0500

    enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
+    preflightContinue: false,
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
